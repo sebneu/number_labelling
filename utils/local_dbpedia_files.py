8857f6388c4396554ab7cf5f8d71471ea6a7df71
neumaier <sebastian.neumaier@wu.ac.at>
2017-03-22 18:54:37 +0100

    fixed corrupted pkl file

--- utils/local_dbpedia_files.py
+++ utils/local_dbpedia_files.py
@@ -3,6 +3,9 @@ import os
 import logging
 import pickle
 
+from utils import dbpedia_access
+
+
 def _store_local(data, filename):
     logging.debug('Store local: ' + filename)
     with open(filename, 'w') as f:
@@ -21,3 +24,9 @@ def local_graph(filename):
 def local_common_types(filename):
     with open(filename + '_common_types.pkl', 'r') as f:
         return pickle.load(f)
+
+
+if __name__ == '__main__':
+    # generate common types pkl
+    dbp = dbpedia_access.DBpedia()
+    _store_local(dbp.most_common_types('<http://dbpedia.org/ontology/activeYearsEndYear>'), 'local/activeYearsEndYear_common_types.pkl')
